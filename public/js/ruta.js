f921a124eaba3601b9eacf3a06c85826cfa432b7
Analia Casas Bautista <analiacasas.acb@gmail.com>
2017-12-13 23:26:44 -0400

    no message

--- public/js/ruta.js
+++ public/js/ruta.js
@@ -3,8 +3,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/*
- * ACÁ SE DECLARA LA RUTA GLOBAL PARA HACER LA PETICIÓN DEL AJAX
- */
-var ruta="http://localhost/depau/public/";
 
+var ruta="http://localhost/depau/public/";
